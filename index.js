e4fb9ae2a981465329153b4c845ead0ba922e5c8
Odima-dev <vincentodima22@gmail.com>
2025-02-20 11:51:56 +0300

    rendering conversions to home page

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ const unitsMass = document.getElementById("units-mass")
 
 convertBtn.addEventListener("click", function() {
     console.log(userInput.value)
-    console.log(length())
-    console.log(volume())
-    console.log(mass())
+    length()
+    volume()
+    mass()
 })
 
 function length() {
     const convertFeet = (userInput.value * 3.281).toFixed(3)
     const convertMeter = (userInput.value / 3.281).toFixed(3)
-    return (`${userInput.value} meters = ${convertFeet} feet | ${userInput.value} feet = ${convertMeter} meters`)    
+    unitsLength.textContent = `${userInput.value} meters = ${convertFeet} feet | ${userInput.value} feet = ${convertMeter} meters`
 }
 
 function volume() {
     const convertGallons = (userInput.value * 0.264).toFixed(3)
     const convertLiters = (userInput.value / 0.264).toFixed(3)
-    return (`${userInput.value} liters = ${convertGallons} gallons | ${userInput.value} gallons = ${convertLiters} liters`)
+    unitsVolume.textContent = `${userInput.value} liters = ${convertGallons} gallons | ${userInput.value} gallons = ${convertLiters} liters`
 }
 
 function mass() {
     const convertPounds = (userInput.value * 2.204).toFixed(3)
     const convertKilograms = (userInput.value / 2.204).toFixed(3)
-    return (`${userInput.value} kilos = ${convertPounds} pounds | ${userInput.value} pounds = ${convertKilograms} kilos`)
+    unitsMass.textContent = `${userInput.value} kilos = ${convertPounds} pounds | ${userInput.value} pounds = ${convertKilograms} kilos`
 }
 
