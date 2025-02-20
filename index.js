f1cf6062fe80bbcf40f8936bc666c9c6ed68a142
Odima-dev <vincentodima22@gmail.com>
2025-02-20 11:54:39 +0300

    rendering conversions to home page

--- index.js
+++ index.js
@@ -17,21 +17,27 @@ convertBtn.addEventListener("click", function() {
     mass()
 })
 
+//conversion function for length
 function length() {
     const convertFeet = (userInput.value * 3.281).toFixed(3)
     const convertMeter = (userInput.value / 3.281).toFixed(3)
+    //rendering conversions to html <p> element
     unitsLength.textContent = `${userInput.value} meters = ${convertFeet} feet | ${userInput.value} feet = ${convertMeter} meters`
 }
 
+//conversion function for volume
 function volume() {
     const convertGallons = (userInput.value * 0.264).toFixed(3)
     const convertLiters = (userInput.value / 0.264).toFixed(3)
+    //rendering conversions to html <p> element
     unitsVolume.textContent = `${userInput.value} liters = ${convertGallons} gallons | ${userInput.value} gallons = ${convertLiters} liters`
 }
 
+//conversion function for mass
 function mass() {
     const convertPounds = (userInput.value * 2.204).toFixed(3)
     const convertKilograms = (userInput.value / 2.204).toFixed(3)
+    //rendering conversions to html <p> element
     unitsMass.textContent = `${userInput.value} kilos = ${convertPounds} pounds | ${userInput.value} pounds = ${convertKilograms} kilos`
 }
 
