00f38da9037bb22e7ae0d18c90fa9e8a9150ccde
Odima-dev <vincentodima22@gmail.com>
2025-02-20 12:29:06 +0300

    creating event listener for clear button

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 const userInput = document.getElementById("user-input")
 const convertBtn = document.getElementById("convert-btn")
+const clearBtn = document.getElementById("clear-btn")
 const unitsLength = document.getElementById("units-length")
 const unitsVolume = document.getElementById("units-volume")
 const unitsMass = document.getElementById("units-mass")
@@ -15,6 +16,13 @@ convertBtn.addEventListener("click", function() {
     length()
     volume()
     mass()
+})
+
+clearBtn.addEventListener("click", function() {
+    userInput.value = ""
+    unitsLength.textContent = ""
+    unitsVolume.textContent = ""
+    unitsMass.textContent = ""
 })
 
 //conversion function for length
