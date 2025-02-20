0a585c2e98b654f1842b33d1d4cb879d236eb271
Odima-dev <vincentodima22@gmail.com>
2025-02-20 10:49:50 +0300

    creating event listener

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const userInput = document.getElementsByID("user-input")
+const convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function() {
+    userInput = userInput.value
+})
